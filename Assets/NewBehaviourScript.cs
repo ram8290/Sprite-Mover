bb0010fb1b74846d4185a3b6de7d977ee614dc8a
RICARDO MARTINEZ <RICMARTI@uat.edu>
2018-05-23 13:33:42 -0700

    second commit
    ship moves in all directions.

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -11,6 +11,41 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			Debug.Log ("left arrow");
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			Debug.Log ("right arrow");
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.UpArrow))
+		{
+			Debug.Log ("up arrow");
+			Vector3 position = this.transform.position;
+			position.y++;
+			this.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.DownArrow))
+		{
+			Debug.Log ("down arrow");
+			Vector3 position = this.transform.position;
+			position.y--;
+			this.transform.position = position;
+		}
+		if (Input.GetKey (KeyCode.Space))
+		{
+			Vector3 position = this.transform.position;
+			position.x = 0;
+			position.y = 0;
+
+		}
 	}
 }
